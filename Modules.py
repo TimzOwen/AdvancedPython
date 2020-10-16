bca196eb0e6b1871ff53a8b1badf6badeb66ab5f
Timz Owen <ktimothy@kabarak.ac.ke>
2020-10-16 22:50:26 +0300

    Update Modules.py
    module sample

--- Modules.py
+++ Modules.py
@@ -1,1 +1,7 @@
-#modules
+#create a file and name it module1.py
+def greeting(name):
+  print("Hello, " + name)
+  
+#now import the module and call the funciton
+import mymodule
+mymodule.greeting("Jonathan")
