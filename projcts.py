23006562b2f1a308e8b31e97fd5d6061faf5abc6
Timz Owen <ktimothy@kabarak.ac.ke>
2020-11-21 04:15:35 +0300

    Update projcts.py
    updated the deck

--- projcts.py
+++ projcts.py
@@ -1,43 +1,3 @@
-#first let's import random procedures since we will be shuffling
-import random
-
-#next, let's start building list holders so we can place our cards in there:
-Card = []
-suits = ["Queen of Hearts", "Diamonds", "Clubs", "Spades"]
-royals = ["J", "Q", "K", "A"]
-deck = []
-
-#now, let's start using loops to add our content:
-for i in range(2,11):
-    Card.append(str(i))
-    #this adds numbers 2-10 and converts them to string data
-
-for j in range(4):
-    Card.append(royals[j])
-    #this will add the royal faces to the cardbase
-
-#cool, it works so far. Now we need to "attach" the suits to it
-#this gets a little tricky
-
-for k in range(4):
-    for l in range(13):
-        card = (Card[l] + " of " + suits[k])
-        #this makes each card, cycling through suits, but first through faces
-        deck.append(card)
-        #this adds the information to the "full deck" we want to make
-#now let's shuffle our deck!
-random.shuffle(deck)
-
-#now let's see the cards!
-# for m in range(52):
-#     print(deck[m])
-
-
-card = Card(12, 4)
-# prints "Queen of hearts"
-print(card)
-
-
 # import modules needed for the program to run
 import random
 
@@ -89,23 +49,66 @@ class GamePlayer(object):
     # create a method to add the cards to the pocket from the deck
     def retreive(self, deck):
         self.pocket.append(deck.retrieveCard())
+        return self
     # check player's card
     def checkPocket(self):
         for playerCards in self.pocket:
             playerCards.displayCards()
+    # method to discard cards like joker if retrieved
+    def discardJokers(self):
+        # return a pop method to remove them because its stated that its not in the deck
+        return self.pocket.pop()
 # cards = Cards("Clubs",6)
 # print(cards.display())
 deck = Deck()
 deck.shuffleDeck()
 
-owen = GamePlayer("OWen")
-owen.retreive(deck)
-print(owen.checkPocket())
+# owen = GamePlayer("OWen")
+# owen.retreive(deck).retreive(deck)
+# print(owen.checkPocket())
 
 # card = deck.retrieveCard()
 # print(card.displayCards())
 
 
+
+# CHECKING CODE FROM ASSIGNMENTS GIVEN
+# Cross check quiz 1
+card = Cards(5, 1)
+# prints "5 of clubs"
+print(card)
+
+# cross check quiz 2
+card = Cards(12, 4)
+# prints "Queen of hearts"
+print(card)
+
+# cross check quiz 3
+deck = Deck()
+# Add the 52 standard cards to the new deck
+deck.add_standard_cards()
+
+# cross check quiz 4
+# Reorder the cards in the deck randomly.
+#  The shuffle method makes use of the randint function
+deck.shuffle()
+
+# cross check quiz 5
+# Create a new tiny Deck of cards called hand, made up
+#   of the last five cards in deck.  The deal method should
+#   also remove those last five cards from "deck".
+hand = deck.deal(5)
+
+# print the cards in the hand
+for card in hand.card_list:
+    print(card)
+
+print("----")
+
+# cross check quiz 6
+# print the remaining cards in the deck
+for card in deck.card_list:
+    print(card)
 
 
 
